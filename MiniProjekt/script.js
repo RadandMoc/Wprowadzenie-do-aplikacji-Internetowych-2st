b151c48fed04f4fb17e7033f59272639d4028704
Radosław Mocarski <mocarski@student.agh.edu.pl>
2024-11-04 13:30:41 +0100

    Update script.js

--- MiniProjekt/script.js
+++ MiniProjekt/script.js
@@ -7,48 +7,6 @@ function toggleDetails(id) {
     }
 }
 
-// Walidacja formularza kontaktowego
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
-
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    if (!email || !message) {
-        alert('Proszę uzupełnić wszystkie pola!');
-        event.preventDefault();
-    }
-
-    if (!emailRegex.test(email)) {
-        alert('Proszę podać poprawny adres e-mail!');
-        event.preventDefault();
-    }
-
-    // Wysłanie formularza
-    fetch('https://formspree.io/f/YOUR_FORM_ID', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ recipient, email, message })
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log('Sukces:', data);
-        alert('Wiadomość została wysłana!');
-    })
-    .catch((error) => {
-        console.error('Błąd:', error);
-        alert('Wystąpił problem z wysyłaniem wiadomości: ' + error.message);
-    });
-});
-
 // Pokazywanie i ukrywanie chmurki
 function showSummary(id) {
     const summary = document.getElementById(id);
