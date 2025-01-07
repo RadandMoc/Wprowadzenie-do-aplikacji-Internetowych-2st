6a40d6633e96810dcc96985b087451035f745958
Radosław Mocarski <mocarski@student.agh.edu.pl>
2025-01-07 12:36:04 +0100

    poprawa bazy danych

--- Projekt/backend/products/models.py
+++ Projekt/backend/products/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    role = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"User {self.username} have role {self.role}"
+
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
@@ -22,3 +32,12 @@ class Review(models.Model):
 
     def __str__(self):
         return f"Review by {self.username} for {self.product.name}"
+    
+class Order(models.Model):
+    user = models.ForeignKey(User, related_name='orders', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='orders', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    order_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order of {self.quantity} {self.product.name} by {self.user.username}"
