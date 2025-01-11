c0332886a0445bf7f5731905ad19b5b917a67c38
Radosław Mocarski <mocarski@student.agh.edu.pl>
2025-01-11 22:23:22 +0100

    komituje grubasa

--- Projekt/backend/products/models.py
+++ Projekt/backend/products/models.py
@@ -32,7 +32,7 @@ class Order(models.Model):
     user = models.ForeignKey(User, related_name='orders', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, related_name='orders', on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
-    order_date = models.DateField(auto_now_add=True)
+    order_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Order of {self.quantity} {self.product.name} by {self.user.username}"
